2778a1046eecd93f3bb0d071387fec5545b9d3d3
sejin\admin <seijin0722@naver.com>
2022-09-02 15:25:36 +0900

    초기 샘플 커밋

---
+++ Spring_Team12_withFront/src/test/java/com/example/spring_team12_withfront/SpringTeam12WithFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_team12_withfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTeam12WithFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
