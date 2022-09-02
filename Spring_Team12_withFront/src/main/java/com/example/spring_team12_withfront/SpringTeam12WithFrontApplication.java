33908d2e0ba53697308a3dc1641d4ea42fc6a4f1
sejin\admin <seijin0722@naver.com>
2022-09-02 16:05:30 +0900

    재커밋

--- Spring_Team12_withFront/src/main/java/com/example/spring_team12_withfront/SpringTeam12WithFrontApplication.java
+++ Spring_Team12_withFront/src/main/java/com/example/spring_team12_withfront/SpringTeam12WithFrontApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringTeam12WithFrontApplication {
+public class SpringTeam12WithfrontApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringTeam12WithFrontApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTeam12WithfrontApplication.class, args);
+	}
 
 }
